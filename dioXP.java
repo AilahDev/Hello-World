f2fcd379f51145d964967a097cb49b037035d28d
AilahDev <n_smartins@outlook.com>
2024-01-16 17:43:15 -0300

    alterações no código

--- dioXP.java
+++ dioXP.java
@@ -5,36 +5,37 @@ public class dioXP {
         Scanner scanner = new Scanner(System.in);
         
         while (true) {
-        System.out.print("digite seu nickname de heroi");    
-        String nickname = scanner.nextLine();
+            System.out.print("Digite seu nickname de heroi ou 'sair' para finalizar: ");    
+            String nickname = scanner.nextLine();
 
-        if (nickname.equalsIgnoreCase("sair")) {
-            break;
-        }
+            if (nickname.equalsIgnoreCase("sair")) {
+                break;
+            }
 
-        System.out.print("informe o XP do seu heroi: ");
-        int xp = scanner.nextInt();
-        scanner.nextLine();
-        String nivel;
+            System.out.print("Informe o XP do seu heroi: ");
+            int xp = scanner.nextInt();
+            scanner.nextLine(); // Consumir a nova linha após o número
+            String nivel;
 
-        if (x´< 1000) {
-            nivel = "Ferro";
-        } else if (xp < 2001) {
-            nivel = "Bronze";
-        } else if (xp < 5001) {
-            nivel = "Prata";
-        } else if (xp < 7001) {
-            nivel = "Ouro";
-        } else if (xp < 8001) {
-            nivel = "Platina";
-        } else if (xp < 9001) {
-            nivel = "Ascendente";
-        } else if (xp < 10001) {
-            nivel = "Imortal";
-        } else {
-            nivel = "Radiante";
-        }
-            System.out.print(nickname + "esta no nivel " + nivel);
+            if (xp < 1000) {
+                nivel = "Ferro";
+            } else if (xp < 2001) {
+                nivel = "Bronze";
+            } else if (xp < 5001) {
+                nivel = "Prata";
+            } else if (xp < 7001) {
+                nivel = "Ouro";
+            } else if (xp < 8001) {
+                nivel = "Platina";
+            } else if (xp < 9001) {
+                nivel = "Ascendente";
+            } else if (xp < 10001) {
+                nivel = "Imortal";
+            } else {
+                nivel = "Radiante";
+            }
+
+            System.out.println(nickname + " esta no nivel " + nivel);
         } 
         scanner.close();
     }
