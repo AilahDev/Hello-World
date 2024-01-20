3bd9a86c16e9f508d8aa8b718fb08d8956a24b05
AilahDev <n_smartins@outlook.com>
2024-01-20 17:37:48 -0300

    atualização

--- exercicios-testes/Teste1.js
+++
@@ -1,8 +0,0 @@
-const posicaoInicial = parseInt(gets());
-const totalPassos = parseInt(gets());
-
-// Calcula a posição final do herói
-const posicaoFinal = posicaoInicial + totalPassos;
-
-// Imprime a posição final
-print("Posicao final do heroi:", posicaoFinal);
